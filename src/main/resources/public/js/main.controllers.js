e66459625380fe5b3de2ff94ce8762a810683b7d
Maison Chaves <maison.chaves@p4sglobal.com>
2017-06-14 15:56:26 -0300

    Dev #829: Fechamento de NF na Movimentação Marítima

--- src/main/resources/public/js/main.controllers.js
+++ src/main/resources/public/js/main.controllers.js
@@ -4,22 +4,40 @@
 
     angular.module('pessoaApp').controller('MainController', MainController);
 
-    MainController.$inject = ['PessoaService'];
+    MainController.$inject = ['PessoaService','$routeParams'];
 
     /**
      * @namespace MainController
      * @desc Main Controller of Challenge App
      * @memberOf PessoaApp
      */
-    function MainController(PessoaService) {
+    function MainController(PessoaService, $routeParams) {
 
         var main = this;
 
         listaPessoas();
 
+        if($routeParams.id) {
+            carregaPessoa();
+        }
+        
+        main.salvaPessoas = salvaPessoas;
+
+        function carregaPessoa() {
+            PessoaService.get({id: $routeParams.id}, function(pessoa) {
+                main.pessoa = pessoa;
+            });
+        }
+
         function listaPessoas() {
             PessoaService.query(function (pessoas) {
                 main.pessoas = pessoas;
+            });
+        }
+
+        function salvaPessoas() {
+            PessoaService.save(main.pessoa, function(pessoa){
+                main.pessoas.push(pessoa);
             });
         }
 
